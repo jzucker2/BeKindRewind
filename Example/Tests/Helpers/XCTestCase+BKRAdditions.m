2794e45f34322b1c4bb1877c9cec58dc45986de2
Jordan Zucker <jordan.zucker@gmail.com>
2016-01-19 16:07:53 -0800

    Started fleshing out helper methods. Need to strengthen

--- Example/Tests/Helpers/XCTestCase+BKRAdditions.m
+++ Example/Tests/Helpers/XCTestCase+BKRAdditions.m
@@ -14,15 +14,32 @@
 @implementation XCTestCase (BKRAdditions)
 
 - (void)assertRequest:(BKRRequest *)request withRequest:(NSURLRequest *)otherRequest extraAssertions:(void (^)(BKRRequest *, NSURLRequest *))assertions {
+    XCTAssertNotNil(request);
+    XCTAssertNotNil(otherRequest);
     
+    if (assertions) {
+        assertions(request, otherRequest);
+    }
 }
 
 - (void)assertResponse:(BKRResponse *)response withResponse:(NSURLResponse *)otherResponse extraAssertions:(void (^)(BKRResponse *, NSURLResponse *))assertions {
-    
+    XCTAssertNotNil(response);
+    XCTAssertNotNil(otherResponse);
+//    XCTAssertEqual(response.statusCode, 200);
+//    NSHTTPURLResponse *castedDataTaskResponse = (NSHTTPURLResponse *)response;
+//    XCTAssertEqualObjects(responseFrame.allHeaderFields, castedDataTaskResponse.allHeaderFields);
+//    XCTAssertEqual(responseFrame.statusCode, castedDataTaskResponse.statusCode);
+    if (assertions) {
+        assertions(response, otherResponse);
+    }
 }
 
 - (void)assertData:(BKRData *)data withData:(NSData *)otherData extraAssertions:(void (^)(BKRData *, NSData *))assertions {
-    
+    XCTAssertNotNil(data);
+    XCTAssertNotNil(otherData);
+    if (assertions) {
+        assertions(data, otherData);
+    }
 }
 
 @end
