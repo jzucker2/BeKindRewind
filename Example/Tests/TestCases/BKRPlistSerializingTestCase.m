af27e4a76aa7962174a2178de31580a0a45503d0
Jordan Zucker <jordan.zucker@gmail.com>
2016-01-21 12:02:45 -0800

    Stubbed out serializing test, need to implement

---
+++ Example/Tests/TestCases/BKRPlistSerializingTestCase.m
@@ -0,0 +1,37 @@
+//
+//  BKRPlistSerializingTestCase.m
+//  BeKindRewind
+//
+//  Created by Jordan Zucker on 1/21/16.
+//  Copyright © 2016 Jordan Zucker. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <BeKindRewind/BKRRecordableCassette.h>
+#import <BeKindRewind/BKRScene.h>
+#import <BeKindRewind/BKRDataFrame.h>
+#import <BeKindRewind/BKRResponseFrame.h>
+#import <BeKindRewind/BKRRequestFrame.h>
+#import "XCTestCase+BKRAdditions.h"
+
+@interface BKRPlistSerializingTestCase : XCTestCase
+
+@end
+
+@implementation BKRPlistSerializingTestCase
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testPlistSerialization {
+
+}
+
+@end
