3d5260cfed36290573d89058e0c52fa74b087eed
Jordan Zucker <jordan.zucker@gmail.com>
2016-01-21 23:37:46 -0800

    Added a playable scene for abstraction

---
+++ BeKindRewind/Core/Scene/BKRPlayableScene.h
@@ -0,0 +1,14 @@
+//
+//  BKRPlayableScene.h
+//  Pods
+//
+//  Created by Jordan Zucker on 1/21/16.
+//
+//
+
+#import "BKRScene.h"
+#import "BKRPlistSerializing.h"
+
+@interface BKRPlayableScene : BKRScene <BKRPlistDeserializer>
+
+@end
---
+++ BeKindRewind/Core/Scene/BKRPlayableScene.m
@@ -0,0 +1,13 @@
+//
+//  BKRPlayableScene.m
+//  Pods
+//
+//  Created by Jordan Zucker on 1/21/16.
+//
+//
+
+#import "BKRPlayableScene.h"
+
+@implementation BKRPlayableScene
+
+@end
