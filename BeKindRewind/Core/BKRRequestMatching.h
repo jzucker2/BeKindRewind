173f7302d65d07f1bd03e4b08dd935739f5058d9
Jordan Zucker <jordan.zucker@gmail.com>
2016-01-23 21:31:30 -0800

    Started plotting out the matcher

--- BeKindRewind/Core/BKRRequestMatching.h
+++ BeKindRewind/Core/BKRRequestMatching.h
@@ -11,15 +11,29 @@
 @class BKRPlayableScene;
 @protocol BKRRequestMatching <NSObject>
 
+// required because it's used to initialize internally. Usually just provide standard init
 + (id<BKRRequestMatching>)matcher;
 
-- (BOOL)hasMatchForRequest:(NSURLRequest *)request withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
 
+// at minimum need to provide a scene for a request. nil means that request goes live
+// alternatively, if we executed a block and passed in a value like background fetch, then
+// we could offer options, like fail, let go live, or provide a mock response
 - (BKRPlayableScene *)matchForRequest:(NSURLRequest *)request withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
 
 @optional
 
-- (NSDictionary *)queryItemsForRequest:(NSURLRequest *)request;
-- (NSString *)hostForRequest:(NSURLRequest *)request;
+// optional values can override this (or should I make it work the opposite of that?)
+- (BOOL)hasMatchForRequest:(NSURLRequest *)request withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+
+// all of these override the one above
+- (BOOL)hasMatchForRequestScheme:(NSString *)scheme withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestUser:(NSString *)user withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestPassword:(NSString *)password withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestPort:(NSNumber *)port withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestFragment:(NSString *)fragment withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestHost:(NSString *)host withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestPath:(NSString *)path withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+- (BOOL)hasMatchForRequestQueryItems:(NSArray<NSURLQueryItem *> *)queryItems withPlayheadIdentifier:(NSString *)playheadIdentifier inPlayableScenes:(NSArray<BKRPlayableScene *> *)scenes;
+
 
 @end
