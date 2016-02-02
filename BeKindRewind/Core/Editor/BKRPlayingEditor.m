28503acdae25a4ca5da8a4f63c134a503f771a99
Jordan Zucker <jordan.zucker@gmail.com>
2016-02-02 15:20:10 -0800

    Minor tweaks, need to rework matching

--- BeKindRewind/Core/Editor/BKRPlayingEditor.m
+++ BeKindRewind/Core/Editor/BKRPlayingEditor.m
@@ -22,45 +22,86 @@
 
 - (void)addStubsForMatcher:(id<BKRRequestMatching>)matcher {
 //    __weak typeof(self) wself = self;
+    // reverse array: http://stackoverflow.com/questions/586370/how-can-i-reverse-a-nsarray-in-objective-c
     NSArray<BKRPlayableScene *> *currentScenes = (NSArray<BKRPlayableScene *> *)self.currentCassette.allScenes;
     dispatch_barrier_async(self.editingQueue, ^{
 //        __weak typeof (wself) sself = wself;
-        for (BKRPlayableScene *stubbingScene in currentScenes) {
+        __block NSUInteger callCount = 0;
+        [currentScenes enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(BKRPlayableScene * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
             [BKROHHTTPStubsWrapper stubRequestPassingTest:^BOOL(NSURLRequest * _Nonnull request) {
-                BOOL finalTestResult = [matcher hasMatchForRequest:request withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+                BOOL finalTestResult = [matcher hasMatchForRequest:request withPlayhead:obj inPlayableScenes:currentScenes];
                 if (!finalTestResult) {
                     return finalTestResult;
                 }
                 NSURLComponents *requestComponents = [NSURLComponents componentsWithString:request.URL.absoluteString];
                 if ([matcher respondsToSelector:@selector(hasMatchForRequestScheme:withPlayhead:inPlayableScenes:)]) {
-                    finalTestResult = [matcher hasMatchForRequestScheme:requestComponents.scheme withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+                    finalTestResult = [matcher hasMatchForRequestScheme:requestComponents.scheme withPlayhead:obj inPlayableScenes:currentScenes];
                     if (!finalTestResult) {
                         return finalTestResult;
                     }
                 }
                 if ([matcher respondsToSelector:@selector(hasMatchForRequestHost:withPlayhead:inPlayableScenes:)]) {
-                    finalTestResult = [matcher hasMatchForRequestHost:requestComponents.host withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+                    finalTestResult = [matcher hasMatchForRequestHost:requestComponents.host withPlayhead:obj inPlayableScenes:currentScenes];
                     if (!finalTestResult) {
                         return finalTestResult;
                     }
                 }
                 if ([matcher respondsToSelector:@selector(hasMatchForRequestPath:withPlayhead:inPlayableScenes:)]) {
-                    finalTestResult = [matcher hasMatchForRequestPath:requestComponents.path withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+                    finalTestResult = [matcher hasMatchForRequestPath:requestComponents.path withPlayhead:obj inPlayableScenes:currentScenes];
                     if (!finalTestResult) {
                         return finalTestResult;
                     }
                 }
                 if ([matcher respondsToSelector:@selector(hasMatchForRequestQueryItems:withPlayhead:inPlayableScenes:)]) {
-                    finalTestResult = [matcher hasMatchForRequestQueryItems:requestComponents.queryItems withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+                    finalTestResult = [matcher hasMatchForRequestQueryItems:requestComponents.queryItems withPlayhead:obj inPlayableScenes:currentScenes];
                     if (!finalTestResult) {
                         return finalTestResult;
                     }
                 }
                 return finalTestResult;
             } withStubResponse:^BKRPlayableScene * _Nonnull(NSURLRequest * _Nonnull request) {
-                return [matcher matchForRequest:request withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+                BKRPlayableScene *matchedScene = [matcher matchForRequest:request withPlayhead:obj inPlayableScenes:currentScenes];
+                callCount++;
+                return matchedScene;
             }];
-        }
+        }];
+//        for (NSUInteger i=0; i < currentScenes.count; i++) {
+//            BKRPlayableScene *stubbingScene = [currentScenes objectAtIndex:i];
+//            [BKROHHTTPStubsWrapper stubRequestPassingTest:^BOOL(NSURLRequest * _Nonnull request) {
+//                BOOL finalTestResult = [matcher hasMatchForRequest:request withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+//                if (!finalTestResult) {
+//                    return finalTestResult;
+//                }
+//                NSURLComponents *requestComponents = [NSURLComponents componentsWithString:request.URL.absoluteString];
+//                if ([matcher respondsToSelector:@selector(hasMatchForRequestScheme:withPlayhead:inPlayableScenes:)]) {
+//                    finalTestResult = [matcher hasMatchForRequestScheme:requestComponents.scheme withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+//                    if (!finalTestResult) {
+//                        return finalTestResult;
+//                    }
+//                }
+//                if ([matcher respondsToSelector:@selector(hasMatchForRequestHost:withPlayhead:inPlayableScenes:)]) {
+//                    finalTestResult = [matcher hasMatchForRequestHost:requestComponents.host withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+//                    if (!finalTestResult) {
+//                        return finalTestResult;
+//                    }
+//                }
+//                if ([matcher respondsToSelector:@selector(hasMatchForRequestPath:withPlayhead:inPlayableScenes:)]) {
+//                    finalTestResult = [matcher hasMatchForRequestPath:requestComponents.path withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+//                    if (!finalTestResult) {
+//                        return finalTestResult;
+//                    }
+//                }
+//                if ([matcher respondsToSelector:@selector(hasMatchForRequestQueryItems:withPlayhead:inPlayableScenes:)]) {
+//                    finalTestResult = [matcher hasMatchForRequestQueryItems:requestComponents.queryItems withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+//                    if (!finalTestResult) {
+//                        return finalTestResult;
+//                    }
+//                }
+//                return finalTestResult;
+//            } withStubResponse:^BKRPlayableScene * _Nonnull(NSURLRequest * _Nonnull request) {
+//                return [matcher matchForRequest:request withPlayhead:stubbingScene inPlayableScenes:currentScenes];
+//            }];
+//        }
     });
 }
 
