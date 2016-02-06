8c3f002054cfb9c944cd090394c2beb62bdc4281
Jordan Zucker <jordan.zucker@gmail.com>
2016-02-06 05:54:28 -0800

    Significantly more documentation, bumped pod to push the new docs

--- BeKindRewind/Core/Editor/BKREditor.h
+++ BeKindRewind/Core/Editor/BKREditor.h
@@ -11,16 +11,39 @@
 @class BKRCassette;
 @class BKRScene;
 
+/**
+ *  This object is responsible for translating BeKindRewind network information between cassettes
+ *  and network objects
+ */
 @interface BKREditor : NSObject
 
+/**
+ *  Convenience initializer
+ *
+ *  @return instance of BKREditor
+ */
 + (instancetype)editor;
 
+/**
+ *  Determines whether editor should be executing
+ */
 @property (nonatomic, getter=isEnabled) BOOL enabled;
+
+/**
+ *  Separate queue for processing editing actions
+ */
 @property (nonatomic) dispatch_queue_t editingQueue;
 
+/**
+ *  Cassette to perform editing actions on, editing will not occur if cassette is nil
+ */
 @property (nonatomic, strong) BKRCassette *currentCassette;
 
-
+/**
+ *  All scenes from cassette in order of creation
+ *
+ *  @return array of ordered scenes
+ */
 - (NSArray<BKRScene *> *)allScenes;
 
 @end
--- BeKindRewind/Core/Scene/BKRScene.h
+++ BeKindRewind/Core/Scene/BKRScene.h
@@ -13,20 +13,78 @@
 @class BKRErrorFrame;
 @class BKRRequestFrame;
 @class BKRResponseFrame;
+
+/**
+ *  This class represents all the information associated with a network request,
+ *  similar to NSURLSessionTask and its subclasses
+ */
 @interface BKRScene : NSObject
 
-//@property (nonatomic, strong) NSMutableArray<BKRFrame *> *frames;
+/**
+ *  Unique identifier created by BeKindRewind for grouping network information into a scene
+ */
 @property (nonatomic, copy) NSString *uniqueIdentifier;
+
+/**
+ *  The clapboard frame is the first frame recorded in the scene, similar to a clapboard
+ *  at the start of scene in a movie.
+ */
 @property (nonatomic, strong, readonly) BKRFrame *clapboardFrame;
 
+/**
+ *  Safely adds edited frame to the scene
+ *
+ *  @param frame edited frame containing a component of a network request
+ */
 - (void)addFrameToFramesArray:(BKRFrame *)frame;
 
+/**
+ *  Ordered (by creation date) array of all frames in scene
+ *
+ *  @return ordered array of BKRFrame objects
+ */
 - (NSArray<BKRFrame *> *)allFrames;
+
+/**
+ *  Ordered (by creation date) array of only data frames in scene
+ *
+ *  @return ordered array of BKRDataFrame objects
+ */
 - (NSArray<BKRDataFrame *> *)allDataFrames;
+
+/**
+ *  Ordered (by creation date) array of only response frames in scene
+ *
+ *  @return ordered array of BKRResponseFrame objects
+ */
 - (NSArray<BKRResponseFrame *> *)allResponseFrames;
+
+/**
+ *  Ordered (by creation date) array of only request frames in scene
+ *
+ *  @return ordered array of BKRRequestFrame objects
+ */
 - (NSArray<BKRRequestFrame *> *)allRequestFrames;
+
+/**
+ *  Ordered (by creation date) array of only error frames in scene
+ *
+ *  @return ordered array of BKRErrorFrame objects
+ */
 - (NSArray<BKRErrorFrame *> *)allErrorFrames;
+
+/**
+ *  Original request used to initiate a network request
+ *
+ *  @return frame containing information related to a NSURLRequest
+ */
 - (BKRRequestFrame *)originalRequest;
+
+/**
+ *  Current request for a network request (in case the server modifies the request, as can happen with a NSURLSessionTask).
+ *
+ *  @return frame containing information related to a NSURLRequest
+ */
 - (BKRRequestFrame *)currentRequest;
 
 
