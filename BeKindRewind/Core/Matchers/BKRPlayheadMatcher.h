5635c966ea9ff4fa18e1b762222a4101fd7579aa
Jordan Zucker <jordan.zucker@gmail.com>
2016-02-06 04:57:41 -0800

    Continued documenting. About halfway through?

--- BeKindRewind/Core/Matchers/BKRPlayheadMatcher.h
+++ BeKindRewind/Core/Matchers/BKRPlayheadMatcher.h
@@ -9,6 +9,14 @@
 #import <Foundation/Foundation.h>
 #import "BKRRequestMatching.h"
 
+/**
+ *  This is a provided simple request matcher for playback.
+ *  It respects ordering of network requests and returns
+ *  stubs in the same order they were originally recorded.
+ *  
+ *  @note this class can easily be subclassed to adjust the rules
+ *  used for matching
+ */
 @interface BKRPlayheadMatcher : NSObject <BKRRequestMatching>
 
 @end
