0a0ba9bc362cb408bc621fad91e96ad494d65777
Jordan Zucker <jordan.zucker@gmail.com>
2016-02-15 20:36:28 -0800

    Added new files, just stubbed

---
+++ BeKindRewind/Core/Cassette/BKRCassette+Playable.m
@@ -0,0 +1,13 @@
+//
+//  BKRCassette+Playable.m
+//  Pods
+//
+//  Created by Jordan Zucker on 2/15/16.
+//
+//
+
+#import "BKRCassette+Playable.h"
+
+@implementation BKRCassette (Playable)
+
+@end
---
+++ BeKindRewind/Core/Cassette/BKRCassette+Recordable.h
@@ -0,0 +1,13 @@
+//
+//  BKRCassette+Recordable.h
+//  Pods
+//
+//  Created by Jordan Zucker on 2/15/16.
+//
+//
+
+#import "BKRCassette.h"
+
+@interface BKRCassette (Recordable)
+
+@end
