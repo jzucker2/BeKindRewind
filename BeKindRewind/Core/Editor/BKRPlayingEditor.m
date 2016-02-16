fed022eee6d5ef29afb044b0f28bc719a199e96f
Jordan Zucker <jordan.zucker@gmail.com>
2016-02-15 20:21:38 -0800

    More tweaks, hopefully this runs with no issues

--- BeKindRewind/Core/Editor/BKRPlayingEditor.m
+++ BeKindRewind/Core/Editor/BKRPlayingEditor.m
@@ -42,9 +42,11 @@
             BKRPlayableCassette *stubbingCassette = (BKRPlayableCassette *)cassette;
             NSArray<BKRScene *> *currentScenes = (NSArray<BKRScene *> *)stubbingCassette.allScenes;
             NSDate *now = [NSDate date];
+            NSLog(@"%s", __PRETTY_FUNCTION__);
             NSLog(@"now: %@ stubbingCassette: %@", now, stubbingCassette);
             NSLog(@"now: %@ currentScenes: %@", now, currentScenes);
-            [self _addStubsForMatcherWithScenes:currentScenes withCompletionHandler:editingBlock enabled:updatedEnabled cassette:cassette];
+//            [self _addStubsForMatcherWithScenes:currentScenes withCompletionHandler:editingBlock enabled:updatedEnabled cassette:cassette];
+            [self _addStubsForMatcher:self.matcher forCassette:stubbingCassette withCompletionHandler:editingBlock];
         } else {
             [self _removeAllStubs];
             if (editingBlock) {
@@ -80,43 +82,44 @@
 //    });
 }
 
-- (void)_addStubsForMatcherWithScenes:(NSArray<BKRScene *> *)scenes withCompletionHandler:(BKRCassetteEditingBlock)completionBlock enabled:(BOOL)currentEnabled cassette:(BKRCassette *)cassette {
-    [self _addStubsForMatcherForMatcher:self.matcher withScenes:scenes enabled:currentEnabled cassette:cassette andCompletionHandler:completionBlock];
-}
-
-- (void)_addStubsForMatcherForMatcher:(id<BKRRequestMatching>)matcher withScenes:(NSArray<BKRScene *> *)scenes enabled:(BOOL)enabled cassette:(BKRCassette *)cassette andCompletionHandler:(BKRCassetteEditingBlock)completionBlock {
+- (void)_addStubsForMatcher:(id<BKRRequestMatching>)matcher forCassette:(BKRPlayableCassette *)cassette withCompletionHandler:(BKRCassetteEditingBlock)completionBlock {
+    NSArray<BKRScene *> *currentScenes = (NSArray<BKRScene *> *)cassette.allScenes;
+    NSDate *now = [NSDate date];
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    NSLog(@"now: %@ cassette: %@", now, cassette);
+    NSLog(@"now: %@ currentScenes: %@", now, currentScenes);
     // reverse array: http://stackoverflow.com/questions/586370/how-can-i-reverse-a-nsarray-in-objective-c
-    if (!scenes.count) {
+    if (!currentScenes.count) {
         return;
     }
     __block NSUInteger callCount = 0;
-    [scenes enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(BKRScene * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    [currentScenes enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(BKRScene * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         [BKROHHTTPStubsWrapper stubRequestPassingTest:^BOOL(NSURLRequest * _Nonnull request) {
-            BOOL finalTestResult = [matcher hasMatchForRequest:request withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+            BOOL finalTestResult = [matcher hasMatchForRequest:request withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:currentScenes];
             if (!finalTestResult) {
                 return finalTestResult;
             }
             NSURLComponents *requestComponents = [NSURLComponents componentsWithString:request.URL.absoluteString];
             if ([matcher respondsToSelector:@selector(hasMatchForRequestScheme:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
-                finalTestResult = [matcher hasMatchForRequestScheme:requestComponents.scheme withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+                finalTestResult = [matcher hasMatchForRequestScheme:requestComponents.scheme withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:currentScenes];
                 if (!finalTestResult) {
                     return finalTestResult;
                 }
             }
             if ([matcher respondsToSelector:@selector(hasMatchForRequestHost:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
-                finalTestResult = [matcher hasMatchForRequestHost:requestComponents.host withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+                finalTestResult = [matcher hasMatchForRequestHost:requestComponents.host withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:currentScenes];
                 if (!finalTestResult) {
                     return finalTestResult;
                 }
             }
             if ([matcher respondsToSelector:@selector(hasMatchForRequestPath:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
-                finalTestResult = [matcher hasMatchForRequestPath:requestComponents.path withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+                finalTestResult = [matcher hasMatchForRequestPath:requestComponents.path withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:currentScenes];
                 if (!finalTestResult) {
                     return finalTestResult;
                 }
             }
             if ([matcher respondsToSelector:@selector(hasMatchForRequestQueryItems:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
-                finalTestResult = [matcher hasMatchForRequestQueryItems:requestComponents.queryItems withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+                finalTestResult = [matcher hasMatchForRequestQueryItems:requestComponents.queryItems withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:currentScenes];
                 if (!finalTestResult) {
                     return finalTestResult;
                 }
@@ -124,12 +127,64 @@
             return finalTestResult;
         } withStubResponse:^BKRScene * _Nonnull(NSURLRequest * _Nonnull request) {
             // check on this increment call to make sure it happens properly
-            return [matcher matchForRequest:request withFirstMatchedIndex:idx currentNetworkCalls:callCount++ inPlayableScenes:scenes];
+            return [matcher matchForRequest:request withFirstMatchedIndex:idx currentNetworkCalls:callCount++ inPlayableScenes:currentScenes];
         }];
     }];
     if (completionBlock) {
-        completionBlock(enabled, cassette);
+        completionBlock(YES, cassette);
     }
 }
+
+//- (void)_addStubsForMatcherWithScenes:(NSArray<BKRScene *> *)scenes withCompletionHandler:(BKRCassetteEditingBlock)completionBlock enabled:(BOOL)currentEnabled cassette:(BKRCassette *)cassette {
+//    [self _addStubsForMatcherForMatcher:self.matcher withScenes:scenes enabled:currentEnabled cassette:cassette andCompletionHandler:completionBlock];
+//}
+//
+//- (void)_addStubsForMatcherForMatcher:(id<BKRRequestMatching>)matcher withScenes:(NSArray<BKRScene *> *)scenes enabled:(BOOL)enabled cassette:(BKRCassette *)cassette andCompletionHandler:(BKRCassetteEditingBlock)completionBlock {
+//    // reverse array: http://stackoverflow.com/questions/586370/how-can-i-reverse-a-nsarray-in-objective-c
+//    if (!scenes.count) {
+//        return;
+//    }
+//    __block NSUInteger callCount = 0;
+//    [scenes enumerateObjectsWithOptions:NSEnumerationReverse usingBlock:^(BKRScene * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        [BKROHHTTPStubsWrapper stubRequestPassingTest:^BOOL(NSURLRequest * _Nonnull request) {
+//            BOOL finalTestResult = [matcher hasMatchForRequest:request withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+//            if (!finalTestResult) {
+//                return finalTestResult;
+//            }
+//            NSURLComponents *requestComponents = [NSURLComponents componentsWithString:request.URL.absoluteString];
+//            if ([matcher respondsToSelector:@selector(hasMatchForRequestScheme:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
+//                finalTestResult = [matcher hasMatchForRequestScheme:requestComponents.scheme withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+//                if (!finalTestResult) {
+//                    return finalTestResult;
+//                }
+//            }
+//            if ([matcher respondsToSelector:@selector(hasMatchForRequestHost:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
+//                finalTestResult = [matcher hasMatchForRequestHost:requestComponents.host withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+//                if (!finalTestResult) {
+//                    return finalTestResult;
+//                }
+//            }
+//            if ([matcher respondsToSelector:@selector(hasMatchForRequestPath:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
+//                finalTestResult = [matcher hasMatchForRequestPath:requestComponents.path withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+//                if (!finalTestResult) {
+//                    return finalTestResult;
+//                }
+//            }
+//            if ([matcher respondsToSelector:@selector(hasMatchForRequestQueryItems:withFirstMatchedIndex:currentNetworkCalls:inPlayableScenes:)]) {
+//                finalTestResult = [matcher hasMatchForRequestQueryItems:requestComponents.queryItems withFirstMatchedIndex:idx currentNetworkCalls:callCount inPlayableScenes:scenes];
+//                if (!finalTestResult) {
+//                    return finalTestResult;
+//                }
+//            }
+//            return finalTestResult;
+//        } withStubResponse:^BKRScene * _Nonnull(NSURLRequest * _Nonnull request) {
+//            // check on this increment call to make sure it happens properly
+//            return [matcher matchForRequest:request withFirstMatchedIndex:idx currentNetworkCalls:callCount++ inPlayableScenes:scenes];
+//        }];
+//    }];
+//    if (completionBlock) {
+//        completionBlock(enabled, cassette);
+//    }
+//}
 
 @end
