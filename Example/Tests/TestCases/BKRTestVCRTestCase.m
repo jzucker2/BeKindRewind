be520adbf460666dc6d52dc18cd8d5646494228a
Jordan Zucker <jordan.zucker@gmail.com>
2016-02-21 22:37:47 -0800

    Setting up basic test case for test subclass

--- Example/Tests/TestCases/BKRTestVCRTestCase.m
+++ Example/Tests/TestCases/BKRTestVCRTestCase.m
@@ -7,9 +7,12 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <BeKindRewind/BKRTestVCR.h>
+#import "BKRBaseTestCase.h"
+#import "XCTestCase+BKRHelpers.h"
 
-@interface BKRTestVCRTestCase : XCTestCase
-
+@interface BKRTestVCRTestCase : BKRBaseTestCase
+@property (nonatomic, strong) BKRTestVCR *vcr;
 @end
 
 @implementation BKRTestVCRTestCase
@@ -22,18 +25,6 @@
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
 }
 
 @end
